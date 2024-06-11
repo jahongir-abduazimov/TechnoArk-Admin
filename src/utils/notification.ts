9b7c49b24e833f004d4cbd05a3b2ce11f7fad9f2
jakhong1r-dev <jahongirabduazimov707@gmail.com>
2024-06-11 08:48:08 +0500

    fix bugs

--- src/utils/notification.ts
+++ src/utils/notification.ts
@@ -1,25 +1,23 @@
 import { Bounce, TypeOptions, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 interface NotificationProp {
-    title: string,
-    type: TypeOptions | undefined
+  title: string;
+  type: TypeOptions | undefined;
 }
 
 const Notification = (props: NotificationProp) => {
-    return (
-        toast(props.title, {
-            position: "top-right",
-            autoClose: 3000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: 'light',
-            type: props.type,
-            transition: Bounce
-        })
-    )
-}
+  return toast(props.title, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    type: props.type,
+    transition: Bounce,
+  });
+};
 
 export default Notification;
